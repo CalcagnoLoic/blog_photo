61cb654a684d46172fe6b9c9977edc9622989473
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2022-06-28 12:13:20 +0200

    Update settings.py

--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
 
 # Default primary key field type
